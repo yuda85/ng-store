b51c2f81fb8cac9975efa0558cabe65a87bb3d01
yehudabu@rafael.co.il <YEHUDABU@rafael.co.il>
2023-03-09 11:04:28 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'store';
+
+  ngOnInit() {
+    fromEvent(window, 'storage').subscribe((data) => {
+      console.log('storage event', data);
+    });
+  }
+
+  onClick() {
+    localStorage.setItem(`${Math.random() * 100}`, `${Math.random() * 100}`);
+  }
 }
